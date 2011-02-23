3f84a2104832fc5e4c44ef7e32f208992678dd45
chunshengster@gmail.com <chunshengster@gmail.com@6b5266ad-a0b4-f12f-d6c6-b9f052f2ced2>
2011-02-23 02:44:31 +0000

    更新parse_vcard方法的返回值，目前返回$this

--- class_vCard.php
+++ class_vCard.php
@@ -526,7 +526,7 @@ class class_vCard {
 //        print_r($this->_builder->getGeo());
         $this->_parser = NULL;
         $this->_builder = NULL;
-        return $this
+        return $this;
     }
 
     /**
